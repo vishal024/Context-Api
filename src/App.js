bff0384356a9c3b9a18d9b79021b57bed2b935c2
vishal024 <38566525+vishal024@users.noreply.github.com>
2022-11-06 15:50:31 +0530

    useMemo and useCallback

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import ComponentE from './ComponentE';
+import Title from './Title';
 import './style.css';
 
 export const UserContext = React.createContext();
 export const channelContext = React.createContext();
 export default function App() {
+  const [add, setAdd] = useState(0);
+  const [minus, setMinus] = useState(100);
+
+  const multiplication = useMemo(
+    ( function multiply ()  {
+      console.log('multiply is called');
+      return add * 10;
+    }),
+    [add]
+  );
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
       <UserContext.Provider value="vishal kushwah">
-          <channelContext.Provider value="Guitar channel">
-              <ComponentE />
-          </channelContext.Provider>
+        <channelContext.Provider value="Guitar channel">
+          <ComponentE />
+        </channelContext.Provider>
       </UserContext.Provider>
+      <button onClick={() => setAdd(add + 1)}>Addition</button>
+      <span>{add}</span>
+      <br />
+      <span>mulitple to add by 10: {multiplication}</span>
+      <br />
+      <button onClick={() => setMinus(minus - 1)}>subtraction</button>
+      <span>{minus}</span>
+
+      <div>
+           <Title title={"Usecallback Demo"}/>
+      </div>
     </div>
   );
 }
